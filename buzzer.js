5c893bae9f77d06d726c023d871ff5a83a84a5bb
Nazanin Delam <nazanin1427@gmail.com>
2015-08-17 00:04:58 -0700

    buzzer.js

---
+++ buzzer.js
@@ -0,0 +1,59 @@
+var five = require("johnny-five"),
+   songs = require('j5-songs'),
+    board, buzzer;
+    
+board = new five.Board({port: "/dev/cu.usbmodem1411"});
+
+
+board.on("ready", function() {
+  //1 - Using j5-songs library
+  var piezo = new five.Piezo(12);
+
+  var song = songs.load('mario-intro');
+  piezo.play(song);
+
+
+  //2 - Normal way
+  /*board.repl.inject({
+    piezo: piezo
+  });
+
+  // Plays a song
+  piezo.play({
+    // song is composed by an array of pairs of notes and beats
+    // The first argument is the note (null means "no note")
+    // The second argument is the length of time (beat) of the note (or non-note)
+    song: [
+      ["C4", 1 / 4],
+      ["D4", 1 / 4],
+      ["F4", 1 / 4],
+      ["D4", 1 / 4],
+      ["A4", 1 / 4],
+      [null, 1 / 4],
+      ["A4", 1],
+      ["G4", 1],
+      [null, 1 / 2],
+      ["C4", 1 / 4],
+      ["D4", 1 / 4],
+      ["F4", 1 / 4],
+      ["D4", 1 / 4],
+      ["G4", 1 / 4],
+      [null, 1 / 4],
+      ["G4", 1],
+      ["F4", 1],
+      [null, 1 / 2]
+    ],
+    tempo: 100
+  });
+
+  // Plays the same song with a string representation
+  piezo.play({
+    // song is composed by a string of notes
+    // a default beat is set, and the default octave is used
+    // any invalid note is read as "no note"
+    song: "C D F D A - A A A A G G G G - - C D F D G - G G G G F F F F - -",
+    beats: 1 / 4,
+    tempo: 100
+  });*/
+
+});
