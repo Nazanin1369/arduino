3a7c7b1860c3936a3d8d7730f2bc9225c6d467de
Nazanin Delam <nazanin1427@gmail.com>
2015-09-13 14:13:51 -0700

    @feat colorPicker interface with socket.io

---
+++ rgbLed.js
@@ -0,0 +1,22 @@
+var five = require('johnny-five'),
+	io   = require('socket.io').listen(8080),
+	led;
+
+five.Board().on("ready", function(){
+	//Initialized RGB LED
+	 led = new five.Led.RGB([9, 10, 11]);
+
+    this.repl.inject({ led: led });
+
+	io.sockets.on("connection", function(socket) {
+        console.log("New connection: " + socket.id);
+        socket.on("changeColor", function(color) {
+
+            console.log("Changing color: " + color);
+            led.color(color)
+        });
+    });
+
+	led.on();
+
+});
