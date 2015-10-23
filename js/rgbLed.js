45901e8db2572cbe8c2b94ec79925e6dee0989f7
Nazanin Delam <nazanin1427@gmail.com>
2015-10-22 21:43:26 -0700

    @feat Halloween Eyes

--- js/rgbLed.js
+++ js/rgbLed.js
@@ -1,21 +1,35 @@
 var five = require('johnny-five'),
-	io   = require('socket.io').listen(8080),
-	led;
+	io   = require('socket.io').listen(8081),
+	right_eye,
+    left_eye,
+    legs;
 
 five.Board().on("ready", function(){
 	//Initialized RGB LED
-	 led = new five.Led.RGB([9, 10, 11]);
+	right_eye = new five.Led.RGB([9, 10, 11]);
+    left_eye = new five.Led.RGB([3, 5, 6]);
+    legs = new five.Servo({
+         pin: 7
+    });
 
-    this.repl.inject({ led: led });
+     legs.sweep();
+    //this.repl.inject({ led: right_eye });
+    //this.repl.inject({ led: left_eye });
 
 	io.sockets.on("connection", function(socket) {
         console.log("New connection: " + socket.id);
-        socket.on("changeColor", function(color) {
-            console.log("Changing color: " + color);
-            led.color(color)
+        socket.on("changeRightEyeColor", function(color) {
+            console.log("Changing right eye color: " + color);
+            right_eye.color(color);
+        });
+        socket.on("changeLeftEyeColor", function(color) {
+            console.log("Changing left eye color: " + color);
+            left_eye.color(color);
         });
     });
 
-	led.on();
+	right_eye.on();
+    left_eye.on();
+
 
 });
